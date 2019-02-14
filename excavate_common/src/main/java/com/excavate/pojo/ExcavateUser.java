0d84218a7755a221458af5ef2d4e592011f756d4
misterWei <mynameisweiyan@gmail.com>
2019-02-14 22:11:12 +0800

    peizhi SPring容器 -author weiyan

--- excavate_common/src/main/java/com/excavate/pojo/ExcavateUser.java
+++ excavate_common/src/main/java/com/excavate/pojo/ExcavateUser.java
@@ -1,8 +1,9 @@
 package com.excavate.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class ExcavateUser {
+public class ExcavateUser implements Serializable {
     private String id;
 
     private String userName;
